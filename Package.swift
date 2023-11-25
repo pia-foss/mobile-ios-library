e8f4b07fd4d504f4409d996e104bd73d43d6afb3
Laura Sempere <laura.sempere@kape.com>
2023-11-26 00:34:36 +0100

    PIA-906: Update PIAWireguard dependency

--- Package.swift
+++ Package.swift
@@ -26,7 +26,7 @@ let package = Package(
       .package(url: "git@github.com:pia-foss/mobile-ios-releases-account.git", exact: "1.4.4"),
       .package(url: "git@github.com:pia-foss/mobile-ios-releases-regions.git", exact: "1.6.2"),
       .package(url: "git@github.com:pia-foss/mobile-ios-openvpn.git", branch: "master"),
-      .package(url: "git@github.com:pia-foss/mobile-ios-wireguard.git", branch: "master"),
+      .package(url: "git@github.com:pia-foss/mobile-ios-wireguard.git", branch: "PIA-906_update_wireguard_lib_v3"),
       .package(url: "https://github.com/hkellaway/Gloss.git", from: "3.1.0"),
       .package(url: "https://github.com/airbnb/lottie-ios.git", from: "3.4.1"),
       .package(url: "https://github.com/huri000/SwiftEntryKit.git", from: "1.0.3"),

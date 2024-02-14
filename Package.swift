6e71ce1a3f564107b1ff12cb11fd67ad6768aaeb
Said Rehouni <said.rehouni@kape.com>
2024-02-14 18:08:31 +0100

    Increased minimum tvos target

--- Package.swift
+++ Package.swift
@@ -1,5 +1,5 @@
 
-// swift-tools-version:5.8
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -9,7 +9,7 @@ let package = Package(
     defaultLocalization: "en",
     platforms: [
         .iOS(.v12),
-        .tvOS(.v16)
+        .tvOS(.v17)
     ],
     products: [
         .library(
